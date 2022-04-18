a7187bf59db872ff80316e4d294e27f823766396
Alex Skorulis <skorulis@gmail.com>
2022-04-18 13:02:09 +1000

    Improve the login page

--- webfe/src/scene/LoginPage.tsx
+++ webfe/src/scene/LoginPage.tsx
@@ -2,12 +2,20 @@ import { Box, Button, Center, Flex, Heading, Input, InputGroup, InputRightElemen
 import { Component } from "react"; 
 import { PageHeader } from "./PageHeader";
 
-export class LoginPage extends Component<{}, {showPassword: Boolean}> {
+type LoginPageState = {
+    showPassword: Boolean
+    email: string
+    password: string
+}
+
+export class LoginPage extends Component<{}, LoginPageState> {
 
     constructor(props: {}) {
         super(props);
-        this.state = {showPassword: false}
+        this.state = {showPassword: false, email: "", password: ""}
         this.handlePasswordClick = this.handlePasswordClick.bind(this);
+        this.emailChanged = this.emailChanged.bind(this);
+        this.passwordChanged = this.passwordChanged.bind(this);
     }
 
     render() {
@@ -27,23 +35,46 @@ export class LoginPage extends Component<{}, {showPassword: Boolean}> {
     }
 
     form() {
-        return <Flex direction="column" textAlign={"center"} boxShadow='md' mt={4} p={4}>
-            <Text>Form here</Text>
-            <Input placeholder='Email' />
-            <InputGroup size='md'>
-                <Input
-                    pr='4.5rem'
-                    type={this.state.showPassword ? 'text' : 'password'}
-                    placeholder='Password'
-                />
-                <InputRightElement width='4.5rem'>
-                    <Button h='1.75rem' size='sm' onClick={this.handlePasswordClick}>
-                    {this.state.showPassword ? 'Hide' : 'Show'}
-                    </Button>
-                </InputRightElement>
-                </InputGroup>
-
+        return <Flex direction="column" textAlign={"center"} boxShadow='md' mt={4} p={4} gap={4}>
+            <Input 
+                placeholder='Email' 
+                value={this.state.email}
+                onChange={this.emailChanged}
+            />
+            {this.passwordField()}
+            <Button colorScheme='blue' isDisabled={!this.canSubmit()}>
+                <Text>Login</Text>
+            </Button>
         </Flex>
+    }
+
+    canSubmit() {
+        return this.state.email.length > 0 && this.state.password.length > 0
+    }
+
+    passwordField() {
+        return <InputGroup size='md'>
+            <Input
+                value={this.state.password}
+                onChange={this.passwordChanged}
+                pr='4.5rem'
+                type={this.state.showPassword ? 'text' : 'password'}
+                placeholder='Password'
+            />
+            <InputRightElement width='4.5rem'>
+                <Button h='1.75rem' size='sm' onClick={this.handlePasswordClick}>
+                {this.state.showPassword ? 'Hide' : 'Show'}
+                </Button>
+            </InputRightElement>
+        </InputGroup>
+    }
+
+    emailChanged(event: React.FormEvent<HTMLInputElement>) {
+        this.setState({ email: event.currentTarget.value })
+    }
+
+    passwordChanged(event: React.FormEvent<HTMLInputElement>) {
+        this.setState({ password: event.currentTarget.value })
     }
 
     handlePasswordClick() {
